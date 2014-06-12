d37333a129428ef3238662b76b3c6d1726940add
Kenji Ohno <kenji.ohno@mac.com>
2014-06-12 23:34:05 +0900

    Initial Commit

---
+++ investiBeacon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  investiBeacon
+//
+//  Created by Kenji Ohno on 2014/06/12.
+//  Copyright (c) 2014年 Macnica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
